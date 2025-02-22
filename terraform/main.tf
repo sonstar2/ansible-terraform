e3d737e14c65bb5cf37d3cd18359db9c0bab27be
Frederick Son <fredson@redhat.com>
2025-02-22 23:22:00 +1100

    Server

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "aws-test-tf-bucket"
     key            = "terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-2"
     # dynamodb_table = "tfstate-lock"
     encrypt        = true
   }
