b8dd99c2a085b74faa8dab0aa2b8dacce4e64c01
Frederick Son <fredson@redhat.com>
2025-02-22 23:00:11 +1100

    backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "aws-test-tf-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "tfstate-lock"
+    # dynamodb_table = "tfstate-lock"
     encrypt        = true
   }
 }
